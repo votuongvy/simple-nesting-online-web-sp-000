fea355dcbd87de7e6222036c7bf0dfa277bc5357
vy vo <tuonggvy.vo@gmail.com>
2019-11-19 04:21:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,7 +135,7 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   
   return programmer_hash
 end
